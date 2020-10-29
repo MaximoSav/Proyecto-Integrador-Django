f40b90e256d5d1b581fb42c06dcf335b8e8d08dc
MaximoSav <maximosavino10@gmail.com>
2020-10-29 15:11:36 -0300

    Adding a few Models to the project

--- core/mercado/admin.py
+++ core/mercado/admin.py
@@ -3,3 +3,6 @@ from .models import *
 # Register your models here.
 
 admin.site.register(Cliente)
+admin.site.register(Producto)
+admin.site.register(Categoria)
+admin.site.register(Divisa)
--- core/mercado/models.py
+++ core/mercado/models.py
@@ -7,3 +7,19 @@ class Cliente(models.Model):
     nombre = models.CharField(max_length=25,)
     telefono = models.BigIntegerField()
     direccion = models.CharField(max_length=40)
+
+class Producto (models.Model):
+    id = models.AutoField(primary_key=True,)
+    nombre = models.CharField(max_length=40,)
+    precio = models.IntegerField()
+    direccion = models.CharField(max_length=60,)
+    stock = models.IntegerField()
+
+class Categoria (models.Model):
+    id = models.AutoField(primary_key=True,) 
+    nombre = models.CharField(max_length = 30,)
+
+class Divisa (models.Model):
+    id = models.AutoField(primary_key=True,)
+    nombre = models.CharField(max_length = 30,)
+    valor = models.FloatField(null = True,)
