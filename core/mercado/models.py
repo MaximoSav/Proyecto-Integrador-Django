04de073c54fd22719f9d55793298818447a505eb
FrankoH298 <francoh298@gmail.com>
2020-11-01 16:54:18 -0300

    configured media and templates, added base template and added ImageField in productos

--- core/mercado/models.py
+++ core/mercado/models.py
@@ -19,9 +19,10 @@ class Producto (models.Model):
     nombre = models.CharField(max_length=40,)
     precio = models.IntegerField()
     stock = models.IntegerField()
+    foto = models.ImageField(max_length=100, upload_to='fotos_productos/', default='fotos_productos/default.png', blank=True,)
 
 class Categoria (models.Model):
-    id = models.AutoField(primary_key=True,) 
+    id = models.AutoField(primary_key=True,)
     nombre = models.CharField(max_length = 30,)
 
 class Divisa (models.Model):
--- core/mercado/urls.py
+++ core/mercado/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path,include
+from .views import BaseView
 
 urlpatterns = [
+    path('', BaseView.as_view(), name='home'),
 ]
