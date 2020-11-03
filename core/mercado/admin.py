1e7b31f0d5ee95f9642ce2cb0888249501c6e36a
MaximoSav <maximosavino10@gmail.com>
2020-11-03 16:16:17 -0300

    Added Fieldsets to Cliente class

--- core/mercado/admin.py
+++ core/mercado/admin.py
@@ -1,26 +1,24 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-'''
+
 class ClienteInline(admin.TabularInline):
     model = Cliente
 
 class DireccionAdmin(admin.ModelAdmin):
     list_display = ['barrio','calle', 'numero']
     inlines = [ClienteInline]
-'''
-class DireccionInline(admin.TabularInline):
-    model = Direccion
 
 class ClienteAdmin(admin.ModelAdmin):
-    list_display = ['nombre','DNI', 'telefono']
-    inlines = [DireccionInline]
+    fieldsets = (
+        ('Datos', {
+            'fields': ('nombre', 'DNI', 'telefono', 'direccion')
 
-
-
+        }),
+    )
 
 admin.site.register(Cliente, ClienteAdmin)
 admin.site.register(Producto)
 admin.site.register(Categoria)
 admin.site.register(Divisa)
-admin.site.register(Direccion,) #DireccionAdmin)
+admin.site.register(Direccion, DireccionAdmin)
