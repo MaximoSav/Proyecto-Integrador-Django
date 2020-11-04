c5a1d61b9c73fdd889c5caacb1ed5748e65a1845
FrankoH298 <francoh298@gmail.com>
2020-11-03 21:08:24 -0300

    Revert "Added CategoriaAdmin"
    This reverts commit 3e20b967a601a1fdc4b3a1c12852c61a96b6cd65.

--- core/mercado/admin.py
+++ core/mercado/admin.py
@@ -9,6 +9,7 @@ class DireccionAdmin(admin.ModelAdmin):
     fieldsets = (
         ('Datos', {
             'fields': ('barrio', 'calle', 'numero')
+
         }),
     )
     list_display = ['barrio','calle', 'numero']
@@ -18,12 +19,13 @@ class ClienteAdmin(admin.ModelAdmin):
     fieldsets = (
         ('Datos', {
             'fields': ('dni', 'nombre', 'telefono', 'direccion')
+
         }),
     )
     search_fields = ['nombre','dni']
     list_display = ['nombre','telefono','dni']
 
-class ProductoAdmin(admin.ModelAdmin):
+class ProductoAdmin (admin.ModelAdmin):
     fieldsets = (
         ('Datos', {
             'fields': ('foto', 'nombre', 'precio', 'stock')
@@ -32,16 +34,8 @@ class ProductoAdmin(admin.ModelAdmin):
     )
     list_display = ['foto','nombre','precio','stock']
 
-class CategoriaAdmin(admin.ModelAdmin):
-    fieldsets = (
-        ('Datos', {
-            'fields': ('nombre',)
-        }),
-    )
-    list_display = ['nombre']
-
 admin.site.register(Cliente, ClienteAdmin)
 admin.site.register(Producto, ProductoAdmin)
-admin.site.register(Categoria, CategoriaAdmin)
+admin.site.register(Categoria)
 admin.site.register(Divisa)
 admin.site.register(Direccion, DireccionAdmin)
