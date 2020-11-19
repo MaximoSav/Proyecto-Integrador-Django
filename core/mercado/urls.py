0612750e2d0dd35bcfffa5d92a05eff5e34adbdb
FrankoH298 <francoh298@gmail.com>
2020-11-19 16:14:33 -0300

    updated style of login

--- core/mercado/urls.py
+++ core/mercado/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('', BaseView),
-    path('dashboard/',views.dashboardView,name="dashboard"),
+    path('acdashboard/',views.dashboardView,name="dashboard"),
     path('accounts/login/',LoginView.as_view(),name="login_url"),
     path('accounts/register/',views.registerView,name="register_url"),
 
