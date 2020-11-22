334f41675c1093ca12e77a45f98dcb0bc366ff49
FrankoH298 <francoh298@gmail.com>
2020-11-22 02:45:03 -0300

    added input email in registerView

--- core/mercado/forms.py
+++ core/mercado/forms.py
@@ -3,10 +3,11 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class RegisterForm(UserCreationForm):
+    email = forms.EmailField(label="Email")
 
     class Meta:
         model = User
-        fields = ('username', 'password1', 'password2',)
+        fields = ('username', 'password1', 'password2', 'email', )
 
 class BaseForm(forms.Form):
     search = forms.CharField(label='Search', max_length=100)
