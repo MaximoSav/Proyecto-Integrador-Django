e959c3551ada834b5687a874befab0afb96eddbc
MaximoSav <maximosavino10@gmail.com>
2020-11-25 22:21:19 -0300

    Added and FINISHED Divisas in Carrito

--- core/cart/templatetags/cart_tags.py
+++ core/cart/templatetags/cart_tags.py
@@ -11,3 +11,11 @@ def multiply(value, arg):
 @register.filter()
 def money_format(value, arg):
     return f"{value}{arg}"
+
+@register.filter()
+def dolar(value, arg):
+    return str(float(value / 85.75) * float(arg))
+
+@register.filter()
+def euro(value, arg):
+    return str(float(value / 96.5) * float(arg))
